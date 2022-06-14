ecc79c0a8eb63bcd88527fe3e5ebda570d31c30a
khoramism <khoramism@gmail.com>
2022-06-14 12:58:08 +0430

    changing to Wav2vec model

--- voice_collector.py
+++ voice_collector.py
@@ -5,11 +5,11 @@ from pyexpat.errors import codes
 import re 
 import matplotlib.pyplot as plt
 from time import sleep
-#from Wav2Vec2LargeHoma5V3 import Wav2Vec2LargeHoma5V3 
+#from Wav2Vec2LargeV3 import Wav2Vec2LargeV3 
 from numpy import asmatrix 
 import os
 
-#wav = Wav2Vec2LargeHoma5V3()
+#wav = Wav2Vec2LargeV3()
 with open('tel_files/result.json', 'r', errors='ignore') as myfile:   
     df = json.load(myfile)
 
